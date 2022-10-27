9c0b64c9c903aeff8a598b0498429b69b31fc73b
Muhammad Imam Choirudin <mimamch28@gmail.com>
2022-10-27 11:11:16 +0700

    update

--- v2/controller/detail.js
+++ v2/controller/detail.js
@@ -6,6 +6,11 @@ const { getMovieByLink } = require("../../utils/scrapper");
 
 exports.getMovieDetail = async (req, res) => {
   try {
+    const { player } = req.query;
+    if (player) {
+      req.body.link += `?player=${player}`;
+    }
+    console.log(req.body.link);
     const movie = await getMovieByLink(req.body.link ?? req.params.link);
     res.status(200).json(successWithData(movie));
   } catch (error) {
